7930e061a786af814f8925bfc82c3293c2f4d468
yurek2003 <132812047+yurek2003@users.noreply.github.com>
2023-05-19 08:41:06 +0300

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -158,3 +158,33 @@ where dea.continent is not null
 
 
 select * from PercentPopulationVaccinated
+
+
+
+
+
+
+
+
+
+-- Queries used for Tableau Project
+----------------------------------------------------
+
+-- 1.
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
+-- 2.
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
